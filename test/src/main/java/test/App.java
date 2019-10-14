4eb87f778ecc4e31fefd92e63698d38270a7c7e7
ningzuokun <ningzuokun@hzed.com>
2019-10-14 11:41:29 +0800

    feat: 注释添加

--- test/src/main/java/test/App.java
+++ test/src/main/java/test/App.java
@@ -5,17 +5,21 @@ package test;
  */
 public class App {
 
-    static
-    {
+    // 静态块类未初始化
+    static {
         System.out.println("ConstClass init!");
+
+        // 解析: 将符号引用（对象未进入内存，可以定位到目标）转为直接引用（对象已进入内存）
+        String a = App.HELLOWORLD;
+        System.out.println( " >>>>> " + a);
     }
-    public static  final String HELLOWORLD = "hello world";
+
+    // 准备： 为变量分配内存（仅static）并设置初始值（由final修饰直接为指定值，否则为变量初始值）
+    public static final String HELLOWORLD = "hello world";
 }
 
-  class NotInitialization
-{
-    public static void main(String[] args)
-    {
-        System.out.println(App.HELLOWORLD);
+class NotInitialization {
+    public static void main(String[] args) {
+        App app = new App();
     }
 }
